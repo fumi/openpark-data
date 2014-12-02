4af17042b16a18aa36c208cf9ca1f3da6853021e
Fumihiro Kato <fumi@fumi.me>
2014-12-02 19:07:20 +0900

    Refactoring

--- bin/convert-playground-equipment.rb
+++ bin/convert-playground-equipment.rb
@@ -7,12 +7,15 @@ require 'csv'
 require 'securerandom'
 
 
-INPUT_FILE="../original/playground-equipment_utf8.csv"
+#INPUT_FILE="../original/playground-equipment_utf8.csv"
+INPUT_FILE="../original/4-kz-doboku_utf8.csv"
 OUTPUT_FILE="../ttl/playground-equipment.ttl"
 
-PARK = RDF::Vocabulary.new("http://yokohama.openpark.jp/parks/")
-EQUIPMENT = RDF::Vocabulary.new("http://yokohama.openpark.jp/equipment/")
+PARK_RESOURCE = RDF::Vocabulary.new("http://yokohama.openpark.jp/parks/")
+EQUIPMENT_RESOURCE = RDF::Vocabulary.new("http://yokohama.openpark.jp/equipment/")
+ORGANIZATION_RESOURCE = RDF::Vocabulary.new("http://yokohama.openpark.jp/organizations/")
 IC = RDF::Vocabulary.new("http://imi.ipa.go.jp/ns/core/210#")
+PARK = RDF::Vocabulary.new("http://yokohama.openpark.jp/ns/park#")
 
 PREFIXES = {
   :rdf => RDF.to_s,
@@ -21,9 +24,11 @@ PREFIXES = {
   :geo => RDF::GEO.to_s,
   :dcterms => RDF::DC.to_s,
   :xsd => RDF::XSD.to_s,
-  :park => PARK.to_s,
-  :equipment => EQUIPMENT.to_s,
-  :ic => IC.to_s
+  :park_resource => PARK_RESOURCE.to_s,
+  :equipment_resource => EQUIPMENT_RESOURCE.to_s,
+  :organization_resource => ORGANIZATION_RESOURCE.to_s,
+  :ic => IC.to_s,
+  :park => PARK.to_s
 }
 
 def to_western_year(year)
@@ -35,6 +40,7 @@ def to_western_year(year)
 end
 
 firstline = true
+id = 1
 
 RDF::Writer.open(OUTPUT_FILE, :prefixes => PREFIXES) do |writer|
   CSV.foreach(INPUT_FILE) do |row|
@@ -42,32 +48,39 @@ RDF::Writer.open(OUTPUT_FILE, :prefixes => PREFIXES) do |writer|
       firstline = false
       next
     end
-    year = to_western_year(row[11])
+    upper_age_limit, lower_age_limit = row[9].split(/〜/) if row[9] and !row[9].empty?
+    equipment_name = row[4].to_s
+    equipment_name << " - #{row[5]}" if row[5] and !row[5].empty?
+    equipment_uri = EQUIPMENT_RESOURCE[id.to_s]
+    park_uri = PARK_RESOURCE[row[3]]
+
     graph = RDF::Graph.new
-    graph.from_ttl("park:#{row[3]} ic:地点_設備 equipment:#{row[4]} .
-                    equipment:#{row[4]} a ic:設備 ;
-                      rdfs:label \"#{row[6]}\"@ja  ;
-                      ic:設備_名称 \"#{row[6]}\"@ja ;
-                      dcterms:identifier \"#{row[4]}\" ;
-                      ic:設備_ID [ a ic:ID ;
-                        ic:値 \"#{row[4]}\" ;
-                      ] ;
-                      ic:設備_設置地点 park:#{row[3]} ;
-                      ic:設備_管理者 [ a ic:組織;
-                        ic:組織_名称 \"#{row[0]}\"@ja ; 
-                        rdfs:label \"#{row[0]}\"@ja ;
-                      ];
-                      dcterms:created \"#{year}\"^^xsd:year ;
-                      ic:設備_設置日 [ a ic:日時 ;
-                        ic:日時_表記-定型 [ a ic:定型日時 ;
-                          ic:定型日時_年 #{year} ;
-                        ];
-                      ] ;
-                      ic:利用者 \"対象年齢: #{row[12]}\" ;
-                      ic:備考 \"#{row[7]}\"@ja ;
-                      .
-                   ", :prefixes => PREFIXES)
+    graph << [park_uri, IC["地点_設備"], equipment_uri]
+    graph << [park_uri, RDF.type, PARK[row[2]]]
+    writer << [PARK[row[2]], RDF.type, RDF::RDFS.Class]
+    writer << [PARK[row[2]], RDF::RDFS.label, RDF::Literal(name, :language => :ja)]
+    writer << [PARK[row[2]], RDF::RDFS.subClassOf, PARK["公園"]]
+    graph << [equipment_uri, RDF.type, IC["設備"]]
+    graph << [equipment_uri, RDF.type, PARK[row[4]]]
+    graph << [PARK[row[4]], RDF.type, RDF::RDFS.Class]
+    graph << [PARK[row[4]], RDF::RDFS.label, RDF::Literal(row[4], :language => :ja)]
+    graph << [equipment_uri, RDF::DC.identifier, RDF::Literal(id)]
+    graph << [equipment_uri, RDF::RDFS.label, RDF::Literal(equipment_name, :language => :ja)]
+    graph << [equipment_uri, IC["設備_名称"], RDF::Literal(equipment_name, :language => :ja)]
+    graph << [equipment_uri, IC["設備_設置地点"], park_uri]
+    graph << [equipment_uri, IC["設備_管理者"], ORGANIZATION_RESOURCE[row[0]]]
+    graph << [equipment_uri, IC["利用者"], RDF::Literal("対象年齢: #{row[9]}", :language => :ja)] if row[9]
+    graph << [equipment_uri, PARK["年齢下限"], RDF::Literal(upper_age_limit, :datatype => RDF::XSD.integer)] if upper_age_limit
+    graph << [equipment_uri, PARK["年齢上限"], RDF::Literal(lower_age_limit, :datatype => RDF::XSD.integer)] if lower_age_limit
+    graph << [equipment_uri, PARK["数量"], RDF::Literal(row[6], :datatype => RDF::XSD.integer)] if row[6]
+    graph << [equipment_uri, PARK["仕様・規格"], RDF::Literal(row[5], :language => :ja)] if row[5]
 
     writer << graph
+    id = id.succ
+  end
+  ["金沢土木事務所", "南部公園緑地事務所"].each do |name|
+    writer << [ORGANIZATION_RESOURCE[name], RDF.type, IC["組織"]]
+    writer << [ORGANIZATION_RESOURCE[name], RDF::RDFS.label, RDF::Literal(name, :language => :ja)]
+    writer << [ORGANIZATION_RESOURCE[name], IC["組織_名称"], RDF::Literal(name, :language => :ja)]
   end
 end
